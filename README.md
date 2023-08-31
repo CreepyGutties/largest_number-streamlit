57957629eb27dfa913ffe05d03949f243c8089d7
rohan kar <imrohankar@gmail.com>
2023-08-31 13:45:43 +0530

    initial_commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# LARGEST NUMBER USING STREAMLIT AND STREAMLIT CLOUD DEPLOYMENT
+* This is a simple hello world app to find the largest number among 3 given numbers.
+* This is WeeK 8 Assignment as part of my TDS course in Diploma in Data Science.
+
+# Tech Stack
+* Streamlit to build web app
+* Python3.10
+* Streamlit Cloud for deployment
+
+# How to execute
+* Clone the repo
+* create a virtual environment or use root
+* install the requirements.txt file to virtual env or root
+* streamlit run app.py
---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+def find_largest(num1, num2, num3):
+    largest = max(num1, num2, num3)
+    return largest
+
+def is_numeric(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def main():
+    st.title("Find the Largest Number")
+    st.write("Enter three numbers and find the largest among them.")
+
+    num1 = st.text_input("Enter the first number:")
+    num2 = st.text_input("Enter the second number:")
+    num3 = st.text_input("Enter the third number:")
+
+    if st.button("Find Largest"):
+        if is_numeric(num1) and is_numeric(num2) and is_numeric(num3):
+            largest_num = find_largest(float(num1), float(num2), float(num3))
+            st.write("The largest number is:", largest_num)
+        else:
+            st.write("Please enter numeric values only.")
+    
+    footer_html = """
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        padding: 5px;
+        font-size: 14px;
+        color: #888888;
+    }
+    </style>
+    <div class="footer">Made by Rohan Kar</div>
+    """
+    st.markdown(footer_html, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
